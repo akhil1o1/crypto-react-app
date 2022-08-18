8b45ba591c637a5d5dcb9a02b9bd78cf5a2add4e
akhil panwar <akhilpanwar180@gmail.com>
2022-08-18 17:56:18 +0530

    dc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import {Switch, Route, Link} from 'react-router-dom';
-import {Typography} from "@mui/material";
+import {Layout, Typography, Space} from "antd";
 
 function App() {
-    return <h1>welcome to crytoverse</h1>
+    return <div className="app">
+        <div className="navbar">
+
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer">
+            
+        </div>
+    </div>
 }
 
 export default App; 
