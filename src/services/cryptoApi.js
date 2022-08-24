bc2b34f0cc60658bcffbf9c3f68fcd398e55f873
akhil panwar <akhilpanwar180@gmail.com>
2022-08-24 22:07:43 +0530

    working on cryptoApi

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,1 +1,22 @@
+import 'dotenv/config';
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoHeaders = {
+    'X-RapidAPI-Key': process.env.COINRANKING_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins"; 
+
+const createRequest = (url) => ({url, headers: cryptoHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: "cryptoApi",
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest("/exchanges")
+        })
+    })
+});
  
