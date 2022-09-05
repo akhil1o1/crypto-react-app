5f8dbc04c41b0fed3e8734c9804799986a947c1b
akhil panwar <akhilpanwar180@gmail.com>
2022-09-05 17:20:22 +0530

    changed some footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return <div className="footer">
     <Typography.Title level={5} style={{color: "white", textAlign:"center"}}>
     CryptoWorld <br/>
-    © All right reserved
+    A build by Akhil Panwar
     </Typography.Title>
     <Space>
         <Link to="/">Home</Link>
